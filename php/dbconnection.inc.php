822c2b9adb44abe28f824af1f62f4dcbb245a703
samuelbohl <50950798+samuelbohl@users.noreply.github.com>
2020-05-28 12:44:24 +0200

    Update dbconnection.inc.php

--- php/dbconnection.inc.php
+++ php/dbconnection.inc.php
@@ -3,8 +3,6 @@
     
 $link = mysqli_connect("localhost:3306", "root", "root", "expensetracker");
 
-//$link = mysqli_connect("db5000130631.hosting-data.io", "dbu347873", "@3xpenceTrack", "dbs125420");
-
 
 if (mysqli_connect_error()) {
 
